973fd5ef40ebe74bbe74e7d71d4df4d9e31da6a0
yuval schwartz <yuvals249@gmail.com>
2019-08-17 16:02:54 +0300

    mobX - exercise 2

--- src/components/Item.js
+++ src/components/Item.js
@@ -19,7 +19,7 @@ class Item extends Component {
       <div className={item.completed ? "crossed" : null}>
         <input type="checkbox" value={item.name} onClick={this.checkItem} />
         {item.name}  {item.location}
-        {/*   your code here
+        {/*   your code here 
             each item should be in an input checkbox
             it should display the item name and location
             it should have both an edit button and a delete button

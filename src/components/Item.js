ea61fc14c5c2e975d83907f0d41f2ee1bb4574a4
yuval schwartz <yuvals249@gmail.com>
2019-08-17 16:02:44 +0300

    mobX - exercise 1

--- src/components/Item.js
+++ src/components/Item.js
@@ -18,7 +18,7 @@ class Item extends Component {
     return (
       <div className={item.completed ? "crossed" : null}>
         <input type="checkbox" value={item.name} onClick={this.checkItem} />
-        {item.name}
+        {item.name}  {item.location}
         {/*   your code here
             each item should be in an input checkbox
             it should display the item name and location

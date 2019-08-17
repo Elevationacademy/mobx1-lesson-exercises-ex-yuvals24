23db309ec1cbd470b6c4b29cc60888b0903b6195
yuval schwartz <yuvals249@gmail.com>
2019-08-17 14:44:19 +0300

    mobX - spot check 1-2

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,25 +3,26 @@ import { observer } from 'mobx-react'
 
 @observer
 class Item extends Component {
-    checkItem = () => {
-        //your code here
-      }
-      editItem = () => {
-        //your code here
-      }
-      deleteItem = () => {
-        //your code here
-      }
-    render() {
-        return (
-            <div className = "">
-      {/*   your code here
+  checkItem = () => {
+    //your code here
+  }
+  editItem = () => {
+    //your code here
+  }
+  deleteItem = () => {
+    //your code here
+  }
+  render() {
+    let item = this.props.item
+    return (
+      <div> {item.name}
+        {/*   your code here
             each item should be in an input checkbox
             it should display the item name and location
             it should have both an edit button and a delete button
-      */} 
-            </div>)
-    }
+      */}
+      </div>)
+  }
 }
 
 export default Item
--- src/stores/ShoppingList.js
+++ src/stores/ShoppingList.js
@@ -4,8 +4,8 @@ import { Item } from './Item'
 
 export class ShoppingList {
     // your code here
-    list = [];
-    length;
+    @observable list = [];
+    @observable length;
     checkItem = () => {
         // your code here
     }
